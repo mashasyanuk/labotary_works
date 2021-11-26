d8807667fb9d3017473676970004dca762aa47a5
mashasyanuk <61414925+mashasyanuk@users.noreply.github.com>
2021-11-26 09:10:00 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int strspn_(char first[5], char second[5]){
+    int i{};
+
+    while(i<=5){
+        int j{};
+        bool okay=false;
+        while(j<=5 && !okay){
+            if(first[i]==second[j]){ //если они совпадают то сравнение прекращается
+                okay=true;
+                break;
+            }
+            j++;
+        }
+
+        if (!okay){
+            return i;
+        }
+        i++;
+    }
+    return -1;
+
+}
+
+int main(){
+    char first[5]="";
+    char second[5]="";
+    int k{};
+    cin >> first;
+    cin >> second;
+    k=strspn_(first, second);
+    if(k==-1){
+        cout << "строки полностью совпадают";
+    }
+    else{
+        cout << k;
+    }
+
+    return 0;
+}
